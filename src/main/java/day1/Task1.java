29db1a1414fd7664ee0317e80cdfab463be1322c
Иванов Н.А <n1kodaber@yahoo.com>
2020-11-30 19:50:09 +0300

    Commit №2
    Common description:
        Solvings of the first day.
    
    Change:
        src/main/java/day1/Task1.java
        src/main/java/day1/Task2.java
        src/main/java/day1/Task3.java
        src/main/java/day1/Task4.java
        src/main/java/day1/Task5.java
        src/main/java/day1/Task6.java
        src/test/java/Tests/AbstractTest.java
        src/test/java/Tests/day1/Day1Test.java

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -1,7 +1,44 @@
 package day1;
 
+import java.util.StringJoiner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        int counter = 0;
+
+        // Решение, чтобы прошёл тест
+        while (counter != 10) {
+
+            System.out.print("JAVA ");
+            counter++;
+        }
+
+        // СПИСОК СВОЕВОЛЬНЫХ РЕШЕНИЙ
+
+        // Решения, чтобы итоговый вывод не заканчивался на пустой пробел
+        // Через "if"
+//        while (counter != 10) {
+//
+//            if (counter == 9) {
+//
+//                // Без пробела на конце
+//                System.out.print("JAVA");
+//                return;
+//            }
+//
+//            System.out.print("JAVA ");
+//            counter++;
+//        }
+
+        // Через "StringJoiner"
+//        StringJoiner joiner = new StringJoiner(" ");
+//        while(counter != 10) {
+//
+//            joiner.add("JAVA");
+//            counter++;
+//        }
+//
+//        System.out.println(joiner);
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -3,5 +3,10 @@ package day1;
 public class Task6 {
     public static void main(String[] args) {
 
+        int k = (int)Math.round(1 + Math.random() * 8);
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d x %d = %d\n", i, k, i * k);
+        }
     }
 }
