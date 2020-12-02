e4459d865c53698ccad113323b912a4330c2ce25
Иванов Н.А <n1kodaber@yahoo.com>
2020-12-02 18:48:54 +0300

    Commit №4
    Common description:
        Solvings of the third day.
    
    Change:
        Task:
    	src/main/java/day3/Task1.java
    	src/main/java/day3/Task2.java
    	src/main/java/day3/Task3.java
    
        Test:
    	src/test/java/Tests/day3/Day3Task1Test.java
    	src/test/java/Tests/day3/Day3Task2Test.java
    	src/test/java/Tests/day3/Day3Task3Test.java

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        double dividend = scanner.nextDouble();
+        double divider = scanner.nextDouble();
+
+        while(0 != divider) {
+            System.out.println(dividend / divider);
+
+            dividend = scanner.nextInt();
+            divider = scanner.nextInt();
+        }
     }
 }
