0a462206347ade43e24718bab7012223b710f294
Иванов Н.А <n1kodaber@yahoo.com>
2020-12-16 21:10:43 +0300

    Commit №16
    Common description:
        Solvings of the sixteenth day.
    
    Add:
        newFile1.txt
        newFile2.txt
    
    Change:
        Tasks and files:
            file1.txt
            src/main/java/day16/Task1.java
            src/main/java/day16/Task2.java
    
        Tests:
            src/test/java/Tests/day16/Day16Task1Test.java
    	src/test/java/Tests/day16/Day16Task2Test.java

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,31 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        String separator = File.separator;
+        String path = String.format("C:%s!Common%sJavaProjects%sJavaMarathon2020%sfile1.txt", separator, separator, separator, separator);
+        File file = new File(path);
+        printResult(file);
+    }
 
+    public static void printResult(File file) {
+        try (Scanner reader = new Scanner(file)) {
+            int sum = 0;
+            int count = 0;
+
+            while(reader.hasNextLine()) {
+                sum += reader.nextInt();
+                ++count;
+            }
+
+            double result = (double)sum / count;
+            System.out.printf(result + " --> %.3f", result);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
-
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,73 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Task2 {
     public static void main(String[] args) {
+        File newFile1;
+        File newFile2;
 
+        try {
+            newFile1 = new File("newFile1.txt");
+            newFile2 = new File("newFile2.txt");
+
+            if (newFile1.createNewFile()) {
+                try (PrintWriter writer = new PrintWriter(newFile1)) {
+                    for (int i = 0; i < 1_000; i++) {
+                        writer.write((int)Math.round(Math.random() * 101) + "\n");
+                    }
+                }
+            }
+
+            if (newFile2.createNewFile()) {
+                try (Scanner reader = new Scanner(newFile1);
+                     PrintWriter writer = new PrintWriter(newFile2)) {
+
+                    List<Integer> listOfTmp = new ArrayList<>();
+                    while(reader.hasNextInt()) {
+                        listOfTmp.add(reader.nextInt());
+
+                        if (listOfTmp.size() == 20) {
+                            int sum = 0;
+                            for (int i = 0; i < 20; i++) {
+                                sum += listOfTmp.get(i);
+                            }
+
+//                            writer.write(String.format("%.2f\n", (double)sum / 20));
+                            writer.write(Math.round(((double) sum / 20) * 100.0) / 100.0 + "\n");
+                            listOfTmp.clear();
+                        }
+                    }
+                }
+            }
+
+            printResult(newFile2);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void printResult(File file) {
+        double result = 0.0;
+
+        try(Scanner reader = new Scanner(file)) {
+            while(reader.hasNextLine()) {
+                List<Double> arr = Arrays.stream(reader.nextLine().split(" ")).map(Double::parseDouble).collect(Collectors.toList());
+                for (Double d : arr)
+                    result += d;
+            }
+
+            System.out.println((int)result);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
